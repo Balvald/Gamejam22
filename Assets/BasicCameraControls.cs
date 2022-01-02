65b439695ae58b79d5afadde2a344cc499cc84c9
Superschnizel <leon.holtmeier@pluto.uni-freiburg.de>
2022-01-02 16:51:36 +0100

    adding input

--- Assets/BasicCameraControls.cs
+++ Assets/BasicCameraControls.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BasicCameraControls : MonoBehaviour
 {
+    [SerializeField]
     public float speed = 5.0f;
     private Vector3 movement = new Vector3(0, 0, -10);
-    
+    private Vector2 mMoveVec;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // set camera movement to 0-vector 2D vector
-        movement = new Vector3(0, 0, -10);
+        // movement = new Vector3(0, 0, -10);
+        transform.position = new Vector3(0,0,-10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.RightArrow))
         {
             movement += new Vector3(speed * Time.deltaTime, 0, 0);
@@ -35,5 +41,22 @@ public class BasicCameraControls : MonoBehaviour
             movement += new Vector3(0, speed * Time.deltaTime, 0);
         }
         transform.position = movement;
+        */
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(mMoveVec.x, mMoveVec.y, 0 ) * speed;
+    }
+
+    public void MoveCamera(InputAction.CallbackContext input)
+    {
+        mMoveVec = input.ReadValue<Vector2>();
+        Debug.Log(mMoveVec);
+    }
+
+    void OnMoveCamera(InputValue input)
+    {
+        mMoveVec = input.Get<Vector2>();
     }
 }
---
+++ Assets/TrainStation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainStation : MonoBehaviour, IHoverable, IClickable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHoverEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHoverExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStartClick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndClick()
+    {
+        throw new System.NotImplementedException();
+    }
+}
