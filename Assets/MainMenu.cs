49ea8663c80336be68cd3f90ef2453ffe8f8989d
Florian Probst <derbalvald@gmail.com>
2022-01-08 02:45:46 +0100

    Credits for the main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,10 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject creditCanvas;
+    public GameObject playButton;
+    public GameObject creditsButton;
+    public GameObject exitButton;
+
+    void Start()
+    {
+        creditCanvas.SetActive(false);
+    }
+
     public void ExitButton()
     {
         Application.Quit();
         Debug.Log("We're done here");
+    }
+
+    public void CreditsButton()
+    {
+        creditCanvas.SetActive(true);
+        playButton.SetActive(false);
+        creditsButton.SetActive(false);
+        exitButton.SetActive(false);
+    }
+
+    public void ReturnFromCreditsButton()
+    {
+        creditCanvas.SetActive(false);
+        playButton.SetActive(true);
+        creditsButton.SetActive(true);
+        exitButton.SetActive(true);
     }
 
     public void StartGame()
